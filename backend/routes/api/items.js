aca46c102950163e4bf147f4b81c06049b7d8434
nickgray06 <nickgray87@gmail.com>
2022-08-14 20:29:41 -0600

    another one

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -53,10 +53,9 @@ router.get("/", auth.optional, function(req, res, next) {
     query.tagList = { $in: [req.query.tag] };
   }
 
-
-  // if (typeof req.query.title !== "undefined") {
-  //   query.titleList = { $in: [req.query.title]}
-  // }
+  if (typeof req.query.title !== "undefined") {
+    query.title = { $in: [req.query.title]}
+  }
 
   Promise.all([
     req.query.seller ? User.findOne({ username: req.query.seller }) : null,
