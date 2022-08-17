6be466fb70682af06c489b70bea948dedb8399eb
nickgray06 <nickgray87@gmail.com>
2022-08-16 21:02:05 -0600

    commit

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -6,8 +6,17 @@ const Banner = () => {
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
+          <div class="input-group">
         <div>
-          <span id="get-part">A place to get</span>
+          <span id="get-part">A Place to get </span>
+  {/* <div id="search-autocomplete" class="form-outline">
+    <input type="search" id="form1" class="form-control" />
+    <label class="form-label" for="form1">Search</label>
+  </div>
+  <button type="button" class="btn btn-primary">
+    <i class="fas fa-search"></i>
+  </button>
+</div> */}
           <span> the cool stuff.</span>
         </div>
       </div>
